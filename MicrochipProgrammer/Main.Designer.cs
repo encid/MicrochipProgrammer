b449ffa2540e8e047297cd2b2720656d243031e6
encid <ryancav@gmail.com>
2017-05-17 15:27:53 -0400

    Initial commit

---
+++ MicrochipProgrammer/Main.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MicrochipProgrammer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSWPartOne = new System.Windows.Forms.TextBox();
+            this.txtSWPartTwo = new System.Windows.Forms.TextBox();
+            this.txtECL = new System.Windows.Forms.TextBox();
+            this.cmdGetECL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSWPartOne
+            // 
+            this.txtSWPartOne.Location = new System.Drawing.Point(68, 50);
+            this.txtSWPartOne.Name = "txtSWPartOne";
+            this.txtSWPartOne.Size = new System.Drawing.Size(78, 20);
+            this.txtSWPartOne.TabIndex = 0;
+            // 
+            // txtSWPartTwo
+            // 
+            this.txtSWPartTwo.Location = new System.Drawing.Point(163, 50);
+            this.txtSWPartTwo.Name = "txtSWPartTwo";
+            this.txtSWPartTwo.Size = new System.Drawing.Size(42, 20);
+            this.txtSWPartTwo.TabIndex = 1;
+            // 
+            // txtECL
+            // 
+            this.txtECL.Location = new System.Drawing.Point(231, 50);
+            this.txtECL.Name = "txtECL";
+            this.txtECL.Size = new System.Drawing.Size(38, 20);
+            this.txtECL.TabIndex = 2;
+            // 
+            // cmdGetECL
+            // 
+            this.cmdGetECL.Location = new System.Drawing.Point(294, 48);
+            this.cmdGetECL.Name = "cmdGetECL";
+            this.cmdGetECL.Size = new System.Drawing.Size(75, 23);
+            this.cmdGetECL.TabIndex = 3;
+            this.cmdGetECL.Text = "Get ECL";
+            this.cmdGetECL.UseVisualStyleBackColor = true;
+            this.cmdGetECL.Click += new System.EventHandler(this.cmdGetECL_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 297);
+            this.Controls.Add(this.cmdGetECL);
+            this.Controls.Add(this.txtECL);
+            this.Controls.Add(this.txtSWPartTwo);
+            this.Controls.Add(this.txtSWPartOne);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSWPartOne;
+        private System.Windows.Forms.TextBox txtSWPartTwo;
+        private System.Windows.Forms.TextBox txtECL;
+        private System.Windows.Forms.Button cmdGetECL;
+    }
+}
+
---
+++ MicrochipProgrammer/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Configuration;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace MicrochipProgrammer
+{
+    public partial class Main : Form
+    {
+        public const string VAULT_PATH = @"\\pandora\vault\Released_Part_Information\240-xxxxx-xx_Software\240-9XXXX-XX\240-91XXX-XX\";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdGetECL_Click(object sender, EventArgs e)
+        {
+            Regex rgx = new Regex(@"/ECL-\w+$/g");
+
+            var softwarePartOne = txtSWPartOne.Text;
+            var softwarePartNumber = string.Format("240-{0}-{1}", txtSWPartOne.Text, txtSWPartTwo.Text);
+
+            var dirList = Directory.GetDirectories(string.Format(@"{0}\240-{1}-XX\{2}", VAULT_PATH, softwarePartOne, softwarePartNumber));
+
+            //foreach (var dir in dirList)
+            //{
+            //    MessageBox.Show(dir);
+            //}
+
+            //var dirECL = dirList.Where(p => p.Contains("ECL")).ToString();
+            //var dirz = from dir in dirList
+            //                where dir.Contains("ECL")
+            //                select dir;
+            string dirECL = "";
+            //foreach (var d in dirz)
+            //{
+            //    dirECL = d;
+            //}
+            foreach (var dir in dirList)
+            {
+                if (dir.Contains("ECL"))
+                    dirECL = dir;
+            }
+
+            //MessageBox.Show(dirECL);
+            var tempECLStr = dirECL.Substring(dirECL.Length - 6);MessageBox.Show(tempECLStr);
+            int dash = tempECLStr.IndexOf("-");
+            var softwareECL = tempECLStr.Substring(dash + 1);
+            txtECL.Text = softwareECL;
+        }
+
+        
+    }
+}
