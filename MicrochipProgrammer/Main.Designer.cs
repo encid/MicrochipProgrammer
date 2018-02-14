570af2caeccd54849482416d5d1bf58990129e0d
encid <ryancav@gmail.com>
2018-02-14 13:18:29 -0500

    1.1 Release -- Rolled rev to 1.1, updated config file.

--- MicrochipProgrammer/Main.Designer.cs
+++ MicrochipProgrammer/Main.Designer.cs
@@ -253,9 +253,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(531, 25);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 13;
-            this.label3.Text = "v1.0, 10/25/17";
+            this.label3.Text = "v1.1, 2/14/18";
             // 
             // Main
             // 
